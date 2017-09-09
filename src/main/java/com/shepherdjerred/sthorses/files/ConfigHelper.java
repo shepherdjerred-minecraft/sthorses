b1d24c019d5830d32b032f614d7f1c0d5a63c67d
Steven M <admin@sigmaroot.de>
2017-09-09 10:19:47 +0200

    Removed outdated methods for configuration files Fixes #1

--- src/main/java/com/shepherdjerred/sthorses/files/ConfigHelper.java
+++ src/main/java/com/shepherdjerred/sthorses/files/ConfigHelper.java
@@ -3,13 +3,14 @@ package com.shepherdjerred.sthorses.files;
 import com.shepherdjerred.sthorses.Main;
 import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.*;
+
 public class ConfigHelper {
 
-    @SuppressWarnings("deprecation")
     public static void loadConfigs() {
 
         Main.getInstance().saveDefaultConfig();
-        Main.getInstance().getConfig().setDefaults(YamlConfiguration.loadConfiguration(Main.getInstance().getResource("config.yml")));
+        Main.getInstance().getConfig().setDefaults(YamlConfiguration.loadConfiguration(new File("config.yml")));
         Main.getInstance().getConfig().options().copyDefaults(true);
         Main.getInstance().saveConfig();
 
