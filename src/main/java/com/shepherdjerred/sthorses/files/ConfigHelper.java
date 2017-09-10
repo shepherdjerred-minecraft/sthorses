2aad6b5b0008bc0eae4e7fbe7172f8ffb80b84c4
Steven M <admin@sigmaroot.de>
2017-09-10 02:10:52 +0200

    Removed config resetting stuff

--- src/main/java/com/shepherdjerred/sthorses/files/ConfigHelper.java
+++ src/main/java/com/shepherdjerred/sthorses/files/ConfigHelper.java
@@ -1,17 +1,12 @@
 package com.shepherdjerred.sthorses.files;
 
 import com.shepherdjerred.sthorses.Main;
-import org.bukkit.configuration.file.YamlConfiguration;
-
-import java.io.*;
 
 public class ConfigHelper {
 
     public static void loadConfigs() {
 
         Main.getInstance().saveDefaultConfig();
-        Main.getInstance().getConfig().setDefaults(YamlConfiguration.loadConfiguration(new File("config.yml")));
-        Main.getInstance().getConfig().options().copyDefaults(true);
         Main.getInstance().saveConfig();
 
         FileManager.getInstance().loadFiles();
